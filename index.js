ca9bfc88768f2d5567369b2c546b910fa7c1ec02
joanarijo <joanarijodesign@gmail.com>
2021-11-16 23:41:28 +0000

    add new window

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
    mainWindow = new BrowserWindow({});
@@ -12,16 +13,36 @@ app.on('ready', () => {
    Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
 const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            {
-                
+            { label: 'New Todo',
+            click() { createAddWindow (); }
+        },
+            { label: 'Quit',
+            accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
             }
+        }
         ]
     },
     {
         label: 'Edit'
     }
 ];
+
+//macOs workaround
+if (process.platform === 'darwin')  {
+    menuTemplate.unshift({});
+}
